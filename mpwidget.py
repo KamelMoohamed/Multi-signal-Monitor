8e24d9a7c9dfcaeffb00276aa2615e948768fbbb
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-11-11 14:55:59 +0200

    feat: adding ui

---
+++ mpwidget.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import *
+
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+
+from matplotlib.figure import Figure
+
+
+class MpWidget(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        fig, ax = plt.subplots()
+        fig.patch.set_facecolor("#f4f5f5")
+        fig.set_facecolor("#ffffff")
+        self.canvas = FigureCanvas(fig)
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
